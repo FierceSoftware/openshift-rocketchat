28d8d1eebd89e9bbb3307ad28a52a8026d2c1f3c
Ken Moini <ken@kenmoini.com>
2019-11-05 19:48:39 -0600

    added token auth ability

--- deployer.sh
+++ deployer.sh
@@ -3,8 +3,12 @@
 ## Default variables to use
 export INTERACTIVE=${INTERACTIVE:="true"}
 export OCP_HOST=${OCP_HOST:=""}
+## userpass or token
+export OCP_AUTH_TYPE=${OCP_AUTH_TYPE:="userpass"}
+export OCP_AUTH=${OCP_AUTH:=""}
 export OCP_USERNAME=${OCP_USERNAME:=""}
 export OCP_PASSWORD=${OCP_PASSWORD:=""}
+export OCP_TOKEN=${OCP_TOKEN:=""}
 export ROCKET_CHAT_ROUTE=${ROCKET_CHAT_ROUTE:="rocketchat.example.com"}
 export OCP_CREATE_PROJECT=${OCP_CREATE_PROJECT:="true"}
 export OCP_PROJECT_NAME=${OCP_PROJECT_NAME:="chatops-rocketchat"}
@@ -22,16 +26,38 @@ if [ "$INTERACTIVE" = "true" ]; then
 		export OCP_HOST="$choice";
 	fi
 
-	read -rp "OpenShift Username: ($OCP_USERNAME): " choice;
+	read -rp "OpenShift Auth Type [userpass or token]: ($OCP_AUTH_TYPE): " choice;
 	if [ "$choice" != "" ] ; then
-		export OCP_USERNAME="$choice";
+		export OCP_AUTH_TYPE="$choice";
 	fi
 
-	read -rsp "OpenShift Password: ($OCP_PASSWORD): " choice;
-	if [ "$choice" != "" ] ; then
-		export OCP_PASSWORD="$choice";
-		echo -e ""
-	fi
+    if [ $OCP_AUTH_TYPE == "userpass" ]; true
+
+        read -rp "OpenShift Username: ($OCP_USERNAME): " choice;
+        if [ "$choice" != "" ] ; then
+            export OCP_USERNAME="$choice";
+        fi
+
+        read -rsp "OpenShift Password: " choice;
+        if [ "$choice" != "" ] ; then
+            export OCP_PASSWORD="$choice";
+        fi
+        echo -e ""
+
+        OCP_AUTH="-u $OCP_USERNAME -p $OCP_PASSWORD"
+
+    fi
+
+    if [ $OCP_AUTH_TYPE == "token" ]; true
+
+        read -rp "OpenShift Token: ($OCP_TOKEN): " choice;
+        if [ "$choice" != "" ] ; then
+            export OCP_TOKEN="$choice";
+        fi
+
+        OCP_AUTH="--token=$OCP_TOKEN"
+
+    fi
 
 	read -rp "Rocket.Chat Route: ($ROCKET_CHAT_ROUTE): " choice;
 	if [ "$choice" != "" ] ; then
@@ -46,8 +72,8 @@ if [ "$INTERACTIVE" = "true" ]; then
 	read -rsp "Rocket.Chat Admin Password: ($ADMIN_PASS): " choice;
 	if [ "$choice" != "" ] ; then
 		export ADMIN_PASS="$choice";
-		echo -e ""
 	fi
+	echo -e ""
 
 	read -rp "Rocket.Chat Admin Email: ($ADMIN_EMAIL): " choice;
 	if [ "$choice" != "" ] ; then
@@ -77,15 +103,15 @@ if [ "$INTERACTIVE" = "true" ]; then
 	read -rsp "Red Hat RHN Password ($RH_PASSWORD): " choice;
 	if [ "$choice" != "" ] ; then
 		export RH_PASSWORD="$choice";
-		echo -e ""
 	fi
+	echo -e ""
 fi
 
-# Log in
+## Log in
 echo "Log in to OpenShift..."
-oc login $OCP_HOST -u $OCP_USERNAME -p $OCP_PASSWORD
+oc login $OCP_HOST $OCP_AUTH
 
-# Create/Use Project
+## Create/Use Project
 echo "Create/Set Project..."
 if [ "$OCP_CREATE_PROJECT" = "true" ]; then
     oc new-project $OCP_PROJECT_NAME --description="ChatOps with Rocket.Chat" --display-name="ChatOps - Rocket.Chat"
@@ -94,19 +120,19 @@ if [ "$OCP_CREATE_PROJECT" = "false" ]; then
     oc project $OCP_PROJECT_NAME
 fi
 
-# Deploy MongoDB
+## Deploy MongoDB
 echo "Deploy MongoDB..."
 oc process -f mongodb-statefulset-replication.yaml | oc apply -f-
 echo "Sleep for 10 seconds..."
 sleep 10
 
-# Create Image Pull Secret
+## Create Image Pull Secret
 echo "Create Image Pull Secret..."
 oc create secret docker-registry rhcc --docker-username=$RH_RHN --docker-password=$RH_PASSWORD --docker-email=$RH_EMAIL --docker-server=registry.connect.redhat.com
 
-# Add secret to default SA
+## Add secret to default SA
 echo "Add Secret to Service Account..."
 oc secrets add serviceaccount/default secrets/rhcc --for=pull
 
-# Deploy Rocket.Chat
+## Deploy Rocket.Chat
 oc process -f rocketchat.yaml -p HOSTNAME_HTTP="$ROCKET_CHAT_ROUTE" -p ACCOUNT_DNS_DOMAIN_CHECK=false -p ADMIN_USERNAME="$ADMIN_USERNAME" -p ADMIN_PASS="$ADMIN_PASS" -p ADMIN_EMAIL="$ADMIN_EMAIL" | oc apply -f-
--- example.vars.sh
+++ example.vars.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 OCP_HOST=""
+
+OCP_AUTH_TYPE="userpass"
+
 OCP_USERNAME=""
 OCP_PASSWORD=""
+OCP_TOKEN=""
 ROCKET_CHAT_ROUTE=""
 OCP_CREATE_PROJECT="true"
 OCP_PROJECT_NAME="chatops-rocketchat"
@@ -14,3 +18,10 @@ ADMIN_PASS="sup3rs3cr3t"
 ADMIN_EMAIL="you@example.com"
 
 INTERACTIVE="false"
+
+if [ $OCP_AUTH_TYPE == "userpass" ]; then
+    OCP_AUTH="-u $OCP_USERNAME -p $OCP_PASSWORD"
+fi
+if [ $OCP_AUTH_TYPE == "token" ]; then
+    OCP_AUTH="--token=$OCP_TOKEN"
+fi
